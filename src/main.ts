8bcbde165815fdd402c3ebedb2bd72752b5e7d76
杨君赫 <yangjunhe@yangjunhedeMacBook-Pro.local>
2019-03-28 10:31:13 +0800

    添加utils

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,19 @@ import Router from 'vue-router';
 import Layout from './wrapper/Layout.vue';
 import routes from './wrapper/router';
 import store from './wrapper/store';
+import * as utils from '@/common/utils';
 
 // util.js是通过store来传入,还是window.super.util
 Vue.config.productionTip = false;
 
+Vue.prototype.$utils = utils;
+
+window.$utils = utils;
+
 Vue.use(Router);
 
 const router = new Router({
-  routes
+  routes,
 });
 router.beforeEach((from: any, to: any, next: any) => {
   if (to.meta && to.meta.title) {
